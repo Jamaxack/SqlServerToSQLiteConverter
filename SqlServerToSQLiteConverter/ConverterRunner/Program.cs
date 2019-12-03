8fde2fcbd8b9a5f68ba1f25fc407df047622d5eb
Jamshed Akhmedov <Jamsheda@silkroadprofessionals.com>
2019-12-03 16:16:03 +0500

    Init project

---
+++ SqlServerToSQLiteConverter/ConverterRunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConverterRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlConnString = @"Data Source=localhost;Initial Catalog=Demo;Integrated Security=SSPI;";
+            string sqlitePath = "C:/Demo.sqlite";
+            string password = null;
+            var excludedTables = new List<string>() { "TableNameThatShouldNotBeInSQLIte" };
+            SqlServerToSQLiteConverter.SqlServerToSQLite.Process(sqlConnString, sqlitePath, password, false, false, excludedTables);
+        }
+    }
+}
